6bd41b2434460999873e4f42e26599799aa47c42
Jade <78552210+Jade-Why@users.noreply.github.com>
2023-11-18 20:07:44 +0000

    Checks if a user's guess is correct

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,19 @@
+from random import choice
+
+word_list = ["pineapple", "mango", "grapes", "strawberries", "kiwi"]
+word = choice(word_list)
+#print(word)
+
 while True:
     guess = input("Please select a letter: ")
     if len(guess) == 1 and guess.isalpha():
         guess = guess.lower()
-        break
+        
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+        break
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
