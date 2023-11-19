428f062d84563551fe094e11f54dd66a42b0c897
Jade <78552210+Jade-Why@users.noreply.github.com>
2023-11-19 16:02:55 +0000

    Adds functions to milestone_3.py for reusability

--- milestone_3.py
+++ milestone_3.py
@@ -1,19 +1,22 @@
 from random import choice
 
-word_list = ["pineapple", "mango", "grapes", "strawberries", "kiwi"]
-word = choice(word_list)
-#print(word)
-
-while True:
-    guess = input("Please select a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        guess = guess.lower()
-        
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
+def check_guess(guess):
+    guess = guess.lower()
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
-        break
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Please select a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+    check_guess(guess)
+           
+
+word_list = ["pineapple", "mango", "grapes", "strawberries", "kiwi"]
+word = choice(word_list)
+print(word)
+
+ask_for_input()
