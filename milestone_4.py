eb881cc77133aaff63632a975289d28a939b0649
Jade <78552210+Jade-Why@users.noreply.github.com>
2023-11-28 17:35:46 +0000

    Adds else block to handle incorrect guesses

--- milestone_4.py
+++ milestone_4.py
@@ -7,30 +7,23 @@ class Hangman:
         
         self.word_guessed = []
         
-        num_letters = len(set(word))
+        self.num_letters = len(set(word))
         self.list_of_guesses = []
 
     def check_guess(self, guess):
         guess = guess.lower()
-        """
-        for letter in word:
-            if letter == guess:
-                current_index = word.index(letter)
-                #print(current_index)
-                print(f"{letter} is at = index: {current_index}")
-                self.word_guessed[current_index] = guess
-                print(self.word_guessed)
-        """
+        #print(f"Number of letters remaining: {self.num_letters}")
+        
         for index, letter in enumerate(word):
-            if letter == guess:
-                print(f"{letter} is at = index: {index}")
-                
+            if letter == guess:                
                 self.word_guessed[index] = guess
                 print(self.word_guessed)
-
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
-
         for i in range(len(word)):
             self.word_guessed.append('_')
         print(self.word_guessed)
@@ -57,14 +50,3 @@ print(word)
 
 game = Hangman(word_list)
 game.ask_for_input()
-
-"""Task 3 implementation:
-Return to your check_guess method and extend it to replace the underscore(s) in the word_guessed with the letter guessed by the user.
-In the if block of your check_guess method, after your print statement, do the following:
-
-Create a for-loop that will loop through each letter in the word /
-Within the for-loop, do the following:
-Create an if statement that checks if the letter is equal to the guess /
-In the if block, replace the corresponding "_" in the word_guessed with the guess. HINT: You can index the word_guessed at the position of the letter and assign it to the letter
-Outside the for-loop, reduce the variable num_letters by 1
-"""
