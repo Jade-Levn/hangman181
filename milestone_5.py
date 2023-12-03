d9ab6314587970e4b61d36cae7bf93e868e360c5
Jade <78552210+Jade-Why@users.noreply.github.com>
2023-12-03 20:03:17 +0000

    Fixes win conditions and allows game to end

--- milestone_5.py
+++ milestone_5.py
@@ -31,10 +31,10 @@ class Hangman:
             print(self.word_guessed)
         else:
             self.num_lives -= 1
-            #num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
             print(self.word_guessed)
+            
 
     def ask_for_input(self):
 
@@ -47,6 +47,13 @@ class Hangman:
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
+                print(f"List of guesses: {self.list_of_guesses}")
+                print(f"Num list of guesses: {len(self.list_of_guesses)},\nNum list of letters: {self.num_letters}")
+                if self.num_lives == 0:
+                    break
+                elif "_" not in self.word_guessed: #and num_letters == 0
+                    break
+            
 
 def play_game(word_list):
     num_lives = 5
@@ -57,18 +64,25 @@ def play_game(word_list):
 
     while True:
         #print(f"Number of lives = {game.num_lives}")
-        if game.num_lives == 0:
+        if game.num_lives <= 0:
+            print("If statement")
             print("You lost!")
             break
         elif game.num_letters > 0:
+            print("First else-if")
             print(f"Number of letters left: {game.num_letters}")
             game.ask_for_input()
-        else:
+            continue
+        elif game.num_lives != 0 and game.num_letters == 0:
+            print("Second else-if")
             print("Congratulations. You won the game!")
             break
+        else:
+            print("An error has occurred.")
+            break
 
-        print(f"Game number of lives 2: {game.num_lives}")
-        print(f"Game number of letters 2: {game.num_letters}")
+    print(f"Game number of lives 2: {game.num_lives}")
+    print(f"Game number of letters 2: {game.num_letters}")
 
 word_list = ["pineapple", "mango", "grapes", "strawberries", "kiwi"]
 
