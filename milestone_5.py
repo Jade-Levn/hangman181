1ebc35b91fbb3e5ec7a958f93f9e7d897d655880
Jade <78552210+Jade-Why@users.noreply.github.com>
2023-12-03 20:39:21 +0000

    Merges changes from play-game branch

--- milestone_5.py
+++ milestone_5.py
@@ -3,38 +3,55 @@ from random import choice
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
-        #num_lives = 5
         self.num_lives = num_lives
 
         self.word = choice(word_list)
-        print(self.word)
+        #print(self.word)
         
         self.word_guessed = []
+        
+        for i in range(len(self.word)):
+            self.word_guessed.append('_')
+        print(self.word_guessed)
         
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
     def check_guess(self, guess):
+        """
+        This method checks to see if a player's guess is within the selected word.
+
+        If the guess is in the word, the underscore is replaced with the correct letter.
+        If the guess is incorrect, the player loses a life.
+        Argument:
+            guess: a string.
+        Returns: 
+            None.
+        """
         guess = guess.lower()
         
         if guess in self.word:                
-            #self.word_guessed[index] = guess
             print(f"Good guess! {guess} is in the word.")
             for index, letter in enumerate(self.word):
                 if letter == guess:
                     self.word_guessed[index] = guess
             self.num_letters -= 1
+            print(self.word_guessed)
         else:
             self.num_lives -= 1
-            #num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
+            print(self.word_guessed)
+            
 
     def ask_for_input(self):
-        for i in range(len(self.word)):
-            self.word_guessed.append('_')
-        print(self.word_guessed)
+        """
+        This method asks the player for input.
 
+        Checks if a user's guess is a single, alphabetical letter.
+        Returns: 
+            None.
+        """
         while True:
             guess = input("Please guess a letter: ")
             if len(guess) != 1 and guess.isalpha():
@@ -44,24 +61,39 @@ class Hangman:
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
+                print(f"List of guesses: {self.list_of_guesses}")
+                print(f"Num list of guesses: {len(self.list_of_guesses)},\nNum list of letters: {self.num_letters}")
+                if self.num_lives == 0:
+                    break
+                elif "_" not in self.word_guessed:
+                    break
+            
 
 def play_game(word_list):
+    """
+    This function runs the Hangman game.
+
+    Creates an instance of the class Hangman. Contains win conditions for the game.
+    Returns: 
+        None.
+    """
     num_lives = 5
     game = Hangman(word_list, num_lives)
 
     while True:
-        #print(f"Number of lives = {game.num_lives}")
         if game.num_lives == 0:
             print("You lost!")
             break
         elif game.num_letters > 0:
             print(f"Number of letters left: {game.num_letters}")
             game.ask_for_input()
+            continue
+        elif game.num_lives > 0 and game.num_letters == 0:
+            print("Congratulations. You won the game!")
             break
         else:
-            print("Congratulations. You won the game!")
+            print("An error has occurred.")
             break
 
 word_list = ["pineapple", "mango", "grapes", "strawberries", "kiwi"]
-
 play_game(word_list)
